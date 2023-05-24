f57c3a3595e6b5fb3c5ce0b60ba046340cb3717d
allz <bakhytzhanov.7@gmail.com>
2023-05-24 15:55:07 +0600

    Deleted extra lines

--- START-HW10-Baqytzhanuly Almaz/ViewController.swift
+++ START-HW10-Baqytzhanuly Almaz/ViewController.swift
@@ -1,20 +1,9 @@
-//
-//  ViewController.swift
-//  START-HW10-Baqytzhanuly Almaz
-//
-//  Created by allz on 5/24/23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         view.backgroundColor = .systemRed
     }
-
-
 }
-
